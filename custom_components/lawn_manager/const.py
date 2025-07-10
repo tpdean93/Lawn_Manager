7e1151123fe7a61d9729f8893a402e9cbeecc096
x_Sno_x <118688486+tpdean93@users.noreply.github.com>
2025-07-10 12:19:13 -0400

    Update const.py

--- custom_components/lawn_manager/const.py
+++ custom_components/lawn_manager/const.py
@@ -1,1 +1,11 @@
+DOMAIN = "lawn_manager"
 
+CHEMICALS = {
+    "Liquid Iron": {"duration_days": 21},
+    "T-Nex": {"duration_days": 225},
+    "Urea": {"duration_days": 30},
+}
+
+DEFAULT_GRASS_TYPES = ["Warm Season", "Cool Season"]
+DEFAULT_ZONES = ["Front Yard", "Back Yard"]
+DEFAULT_APPLICATORS = ["Sprayer", "Spreader"]
