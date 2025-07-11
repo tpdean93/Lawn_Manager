026696beb4dea9744e2ac5af7aa2e99b5939b1a8
x_Sno_x <118688486+tpdean93@users.noreply.github.com>
2025-07-11 15:42:11 -0400

    Update config_flow.py

--- custom_components/lawn_manager/config_flow.py
+++ custom_components/lawn_manager/config_flow.py
@@ -1,19 +1,14 @@
 from homeassistant import config_entries
 import voluptuous as vol
 from homeassistant.data_entry_flow import FlowResult
+from homeassistant.helpers.storage import Store
 
-from .const import DOMAIN, CHEMICALS
-
-CHEMICAL_OPTIONS = list(CHEMICALS.keys())
-METHOD_OPTIONS = ["Sprayer", "Spreader"]
+from .const import DOMAIN
 
 STEP_USER_DATA_SCHEMA = vol.Schema({
     vol.Required("yard_zone"): str,
     vol.Required("location"): str,
     vol.Required("mow_interval", default=7): vol.All(vol.Coerce(int), vol.Range(min=1, max=30)),
-    vol.Optional("chemical"): vol.In(CHEMICAL_OPTIONS),
-    vol.Optional("custom_chemical"): str,
-    vol.Required("method", default="Sprayer"): vol.In(METHOD_OPTIONS),
 })
 
 
@@ -24,28 +19,14 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
 
         if user_input is not None:
-            chemical = user_input.get("chemical")
-            custom_chemical = user_input.get("custom_chemical")
-
-            # Validation: require one or the other
-            if not chemical and not custom_chemical:
-                errors["chemical"] = "required"
-                errors["custom_chemical"] = "required"
-            else:
-                # Prefer custom if provided
-                if custom_chemical:
-                    user_input["chemical"] = custom_chemical
-
-                return self.async_create_entry(
-                    title=user_input["yard_zone"],
-                    data={
-                        "yard_zone": user_input["yard_zone"],
-                        "location": user_input["location"],
-                        "mow_interval": user_input["mow_interval"],
-                        "chemical": user_input["chemical"],
-                        "method": user_input["method"]
-                    }
-                )
+            return self.async_create_entry(
+                title=user_input["yard_zone"],
+                data={
+                    "yard_zone": user_input["yard_zone"],
+                    "location": user_input["location"],
+                    "mow_interval": user_input["mow_interval"],
+                }
+            )
 
         return self.async_show_form(
             step_id="user",
