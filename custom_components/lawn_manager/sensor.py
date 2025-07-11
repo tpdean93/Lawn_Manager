7a4aa8c3ec31d4c1d27c9294423d7673fb6fa7f2
x_Sno_x <118688486+tpdean93@users.noreply.github.com>
2025-07-11 08:01:31 -0400

    Update sensor.py

--- custom_components/lawn_manager/sensor.py
+++ custom_components/lawn_manager/sensor.py
@@ -1,22 +1,43 @@
-from homeassistant.helpers.entity import Entity
 from datetime import datetime, timedelta
+import logging
 
-class ChemicalApplicationSensor(Entity):
-    """A sensor for tracking chemical applications."""
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.const import CONF_NAME
+from homeassistant.helpers.entity import Entity
 
-    def __init__(self, name, chemical, application_date, duration_days, zone):
-        self._name = name
-        self._chemical = chemical
-        self._application_date = application_date
-        self._duration_days = duration_days
-        self._zone = zone
-        self._state = self.calculate_days_remaining()
+from .const import DOMAIN, CONF_LOCATION, CONF_MOW_INTERVAL, DEFAULT_NAME, DEFAULT_MOW_INTERVAL
 
-    def calculate_days_remaining(self):
-        """Calculate how many days are left until the chemical expires."""
-        end_date = self._application_date + timedelta(days=self._duration_days)
-        remaining = (end_date - datetime.now()).days
-        return max(remaining, 0)
+_LOGGER = logging.getLogger(__name__)
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the Lawn Manager sensor platform from YAML config."""
+    name = config.get(CONF_NAME, DEFAULT_NAME)
+    location = config.get(CONF_LOCATION, "Unknown")
+    mow_interval = config.get(CONF_MOW_INTERVAL, DEFAULT_MOW_INTERVAL)
+
+    last_mow_date = hass.states.get(f\"sensor.{DOMAIN}_last_mow_date\")
+    if last_mow_date:
+        last_mow_str = last_mow_date.state
+        try:
+            last_mow = datetime.strptime(last_mow_str, \"%Y-%m-%d\")
+        except Exception:
+            last_mow = datetime.now() - timedelta(days=mow_interval + 1)
+    else:
+        last_mow = datetime.now() - timedelta(days=mow_interval + 1)
+
+    add_entities([
+        LawnMowSensor(name, location, last_mow, mow_interval)
+    ])
+
+class LawnMowSensor(SensorEntity):
+    """Sensor for time since last mowing."""
+
+    def __init__(self, name, location, last_mow, mow_interval):
+        self._name = f\"{name} Mow Interval\"
+        self._location = location
+        self._last_mow = last_mow
+        self._mow_interval = mow_interval
+        self._state = None
 
     @property
     def name(self):
@@ -24,12 +45,15 @@ class ChemicalApplicationSensor(Entity):
 
     @property
     def state(self):
-        return self._state
+        days_since = (datetime.now() - self._last_mow).days
+        return days_since
 
     @property
     def extra_state_attributes(self):
+        next_due = self._last_mow + timedelta(days=self._mow_interval)
         return {
-            "chemical": self._chemical,
-            "application_date": self._application_date.isoformat(),
-            "zone": self._zone,
+            \"location\": self._location,
+            \"last_mow\": self._last_mow.strftime(\"%Y-%m-%d\"),
+            \"next_mow_due\": next_due.strftime(\"%Y-%m-%d\"),
+            \"mow_interval_days\": self._mow_interval,
         }
