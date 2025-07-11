ad3bda518974e935899abe840bcc351a1c80b62f
x_Sno_x <118688486+tpdean93@users.noreply.github.com>
2025-07-11 15:42:38 -0400

    Update sensor.py

--- custom_components/lawn_manager/sensor.py
+++ custom_components/lawn_manager/sensor.py
@@ -6,6 +6,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.storage import Store
 from homeassistant.helpers.entity import EntityCategory
 from homeassistant.util import dt as dt_util
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
 
 from .const import DOMAIN, DEFAULT_MOW_INTERVAL
 
@@ -14,21 +15,70 @@ STORAGE_KEY = "lawn_manager_data"
 STORAGE_VERSION = 1
 
 
+class LawnManagerSensorManager:
+    def __init__(self, hass, entry, async_add_entities):
+        self.hass = hass
+        self.entry = entry
+        self.async_add_entities = async_add_entities
+        self.known_chemicals = set()
+        self.chemical_sensors = {}
+        self.mow_sensor = None
+        self._unsub_dispatcher = None
+
+    async def async_setup(self):
+        store = Store(self.hass, STORAGE_VERSION, STORAGE_KEY)
+        data = await store.async_load() or {}
+        config = self.entry.data
+        yard_zone = config.get("yard_zone", "Lawn")
+        location = config.get("location", "Unknown")
+        mow_interval = config.get("mow_interval", DEFAULT_MOW_INTERVAL)
+
+        # Add mow sensor
+        self.mow_sensor = LawnMowSensor(self.entry.entry_id, yard_zone, location, mow_interval, store)
+        entities = [self.mow_sensor]
+
+        # Add chemical sensors
+        for chem_name, chem_data in data.get("applications", {}).items():
+            self.known_chemicals.add(chem_name)
+            sensor = ChemicalApplicationSensor(self.entry.entry_id, chem_name, chem_data)
+            self.chemical_sensors[chem_name] = sensor
+            entities.append(sensor)
+
+        self.async_add_entities(entities, update_before_add=True)
+
+        # Listen for updates
+        self._unsub_dispatcher = async_dispatcher_connect(self.hass, "lawn_manager_update", self._handle_update_signal)
+
+    async def _handle_update_signal(self):
+        store = Store(self.hass, STORAGE_VERSION, STORAGE_KEY)
+        data = await store.async_load() or {}
+        current_chems = set(data.get("applications", {}).keys())
+        new_chems = current_chems - self.known_chemicals
+        removed_chems = self.known_chemicals - current_chems
+        new_entities = []
+
+        # Add new chemical sensors
+        for chem_name in new_chems:
+            chem_data = data["applications"][chem_name]
+            sensor = ChemicalApplicationSensor(self.entry.entry_id, chem_name, chem_data)
+            self.chemical_sensors[chem_name] = sensor
+            new_entities.append(sensor)
+            self.known_chemicals.add(chem_name)
+
+        if new_entities:
+            self.async_add_entities(new_entities)
+
+        # Remove deleted chemical sensors
+        for chem_name in removed_chems:
+            sensor = self.chemical_sensors.pop(chem_name, None)
+            if sensor:
+                await sensor.async_remove()
+            self.known_chemicals.remove(chem_name)
+
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
-    config = entry.data
-    yard_zone = config.get("yard_zone", "Lawn")
-    location = config.get("location", "Unknown")
-    mow_interval = config.get("mow_interval", DEFAULT_MOW_INTERVAL)
-
-    store = Store(hass, STORAGE_VERSION, STORAGE_KEY)
-    data = await store.async_load() or {}
-
-    entities = [LawnMowSensor(entry.entry_id, yard_zone, location, mow_interval, store)]
-
-    for chem_name, chem_data in data.get("applications", {}).items():
-        entities.append(ChemicalApplicationSensor(entry.entry_id, chem_name, chem_data))
-
-    async_add_entities(entities, update_before_add=True)
+    manager = LawnManagerSensorManager(hass, entry, async_add_entities)
+    await manager.async_setup()
 
 
 class LawnMowSensor(SensorEntity):
@@ -39,6 +89,21 @@ class LawnMowSensor(SensorEntity):
         self._mow_interval = mow_interval
         self._last_mow = None
         self._store = store
+        self._unsub_dispatcher = None
+
+    async def async_added_to_hass(self):
+        self._unsub_dispatcher = async_dispatcher_connect(
+            self.hass, "lawn_manager_update", self._handle_update_signal
+        )
+
+    async def async_will_remove_from_hass(self):
+        if self._unsub_dispatcher:
+            self._unsub_dispatcher()
+            self._unsub_dispatcher = None
+
+    async def _handle_update_signal(self):
+        await self.async_update()
+        self.async_write_ha_state()
 
     async def async_update(self):
         data = await self._store.async_load() or {}
@@ -97,6 +162,21 @@ class ChemicalApplicationSensor(SensorEntity):
         self._amount_oz = chem_data.get("amount_oz_per_1000sqft", 16.0)
         self._method = chem_data.get("method", "Unknown")
         self._state = None
+        self._unsub_dispatcher = None
+
+    async def async_added_to_hass(self):
+        self._unsub_dispatcher = async_dispatcher_connect(
+            self.hass, "lawn_manager_update", self._handle_update_signal
+        )
+
+    async def async_will_remove_from_hass(self):
+        if self._unsub_dispatcher:
+            self._unsub_dispatcher()
+            self._unsub_dispatcher = None
+
+    async def _handle_update_signal(self):
+        await self.async_update()
+        self.async_write_ha_state()
 
     async def async_update(self):
         if not self._last_applied:
