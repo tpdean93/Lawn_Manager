dbf7a264b4b89ff09996e56e1208d47fd3fff6e7
x_Sno_x <118688486+tpdean93@users.noreply.github.com>
2025-07-11 07:59:25 -0400

    Update __init__.py

--- custom_components/lawn_manager/__init__.py
+++ custom_components/lawn_manager/__init__.py
@@ -1,1 +1,13 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+import logging
 
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the Lawn Manager integration from YAML."""
+    _LOGGER.debug("Setting up Lawn Manager via YAML config")
+    return True
