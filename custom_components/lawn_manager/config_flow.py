7c3feb58fadb0d529950a6c12d12c2f82ad6ac47
x_Sno_x <118688486+tpdean93@users.noreply.github.com>
2025-07-12 00:21:13 -0400

    Update config_flow.py

--- custom_components/lawn_manager/config_flow.py
+++ custom_components/lawn_manager/config_flow.py
@@ -3,13 +3,7 @@ import voluptuous as vol
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers.storage import Store
 
-from .const import DOMAIN
-
-STEP_USER_DATA_SCHEMA = vol.Schema({
-    vol.Required("yard_zone"): str,
-    vol.Required("location"): str,
-    vol.Required("mow_interval", default=7): vol.All(vol.Coerce(int), vol.Range(min=1, max=30)),
-})
+from .const import DOMAIN, GRASS_TYPE_LIST
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -25,11 +19,36 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     "yard_zone": user_input["yard_zone"],
                     "location": user_input["location"],
                     "mow_interval": user_input["mow_interval"],
+                    "weather_entity": user_input.get("weather_entity"),
+                    "grass_type": user_input.get("grass_type", "Bermuda"),
                 }
             )
 
+        # Get available weather entities
+        weather_entities = []
+        for entity_id in self.hass.states.async_entity_ids("weather"):
+            state = self.hass.states.get(entity_id)
+            if state:
+                friendly_name = state.attributes.get("friendly_name", entity_id)
+                weather_entities.append((entity_id, friendly_name))
+
+        # Build schema
+        schema_dict = {
+            vol.Required("yard_zone"): str,
+            vol.Required("location"): str,
+            vol.Required("mow_interval", default=7): vol.All(vol.Coerce(int), vol.Range(min=1, max=30)),
+            vol.Required("grass_type", default="Bermuda"): vol.In(GRASS_TYPE_LIST),
+        }
+
+        # Add weather entity selector if weather entities are available
+        if weather_entities:
+            weather_options = [("", "None")] + weather_entities
+            schema_dict[vol.Optional("weather_entity", default="")] = vol.In({k: v for k, v in weather_options})
+
+        data_schema = vol.Schema(schema_dict)
+
         return self.async_show_form(
             step_id="user",
-            data_schema=STEP_USER_DATA_SCHEMA,
+            data_schema=data_schema,
             errors=errors
         )
